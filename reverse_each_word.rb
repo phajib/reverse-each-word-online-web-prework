9a56099dfb69f612f88e2a1d546e54d9af860315
H B <phajib@gmail.com>
2019-01-20 00:00:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  array.each {|string| array << string.reverse}
+def reverse_each_word(str)
+  array = str.split(" ")
+  array.each {|string| array << str.reverse}
   return array.join(" ")
 end
