1652406c427ce19db9d15325c610b9e0b3322889
H B <phajib@gmail.com>
2019-01-20 00:11:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,6 @@ def reverse_each_word(str)
   collect_array = []
   array.each {|str| each_array << str.reverse}
   return each_array.join(" ")
-  #array.collect {|str| collect_array << str.reverse}
-  #return collect_array.join(" ")
+  array.collect {|str| collect_array << str.reverse}
+  return collect_array.join(" ")
 end
