9a3ad4ee99c4bdb8df35b9f896850e3e5cd9eecb
H B <phajib@gmail.com>
2019-01-20 00:08:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(str)
   array = str.split(" ")
   new_array = []
-  array.each {|str| new_array << str.reverse}
+  array.collect {|str| new_array << str.reverse}
   return new_array.join(" ")
 end
