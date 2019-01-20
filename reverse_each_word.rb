716835e9fb8a735178070086c33345e576f8c8dd
H B <phajib@gmail.com>
2019-01-20 00:10:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(str)
   array = str.split(" ")
   each_array = []
   collect_array = []
-  array.each {|str| each_array << str.reverse}
-  return new_array.join(" ")
+  #array.each {|str| each_array << str.reverse}
+  #return new_array.join(" ")
   array.collect {|str| collect_array << str.reverse}
   return collect_array.join(" ")
 end
